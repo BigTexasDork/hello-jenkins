661a7b68587163dd4bf8834f7a100fbdd2eca522
Joe Franklin <joe.franklin@gmail.com>
2015-08-26 21:13:15 -0500

    added properties, endpoint for version

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var propsReader = require('properties-reader');
+var props = propsReader('version.properties');
  
 var app = express();
  
 app.get('/', function (req, res) {
   res.send('hello world from jenkins!');
+});
+
+app.get('/version/', function (req, res) {
+  res.send(props.get('version'));
 });
  
 app.listen(process.env.PORT || 5000);
